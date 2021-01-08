018024de3ccfd5528b7cfe878181e1c5844b61a3
rodrigolampier <rodrigolampier@outlook.com>
2021-01-08 18:22:42 -0300

    Melhora a implementação do view dos Posts.

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
 from .models import Post
@@ -6,21 +7,19 @@ from django.db.models import Q, Count, Case, When
 from comentarios.forms import FormComentario
 from comentarios.models import Comentario
 from django.contrib import messages
+from django.views import View
 
 
 class PostIndex(ListView):
     model = Post
     template_name = 'posts/index.html'
-    paginate_by = 5
+    paginate_by = 6
     context_object_name = 'posts'
 
-    # Ordena os post em ordem decrescente pelo campo ID
     def get_queryset(self):
         qs = super().get_queryset()
-        qs = qs.select_related('categoria_post')  # Permite buscar as categorias dos posts com apenas um única consulta
+        qs = qs.select_related('categoria_post')
         qs = qs.order_by('-id').filter(publicado_post=True)
-        # Só conta os comentário com True em publicar
-        # numero_comentarios é um atributo novo que vai pro html
         qs = qs.annotate(
             numero_comentarios=Count(
                 Case(
@@ -28,6 +27,7 @@ class PostIndex(ListView):
                 )
             )
         )
+
         return qs
 
 
@@ -36,7 +36,6 @@ class PostBusca(PostIndex):
 
     def get_queryset(self):
         qs = super().get_queryset()
-
         termo = self.request.GET.get('termo')
 
         if not termo:
@@ -69,28 +68,62 @@ class PostCategoria(PostIndex):
         return qs
 
 
-class PostDetalhes(UpdateView):
+class PostDetalhes(View):
     template_name = 'posts/post_detalhes.html'
-    model = Post
-    form_class = FormComentario
-    context_object_name = 'post'
 
-    def get_context_data(self, **kwargs):
-        contexto = super().get_context_data(**kwargs)
-        post = self.get_object()
-        comentarios = Comentario.objects.filter(publicado_comentario=True,
-                                                post_comentario=post.id)
-        contexto['comentarios'] = comentarios
-        return contexto
+    def setup(self, request, *args, **kwargs):
+        super().setup(request, *args, **kwargs)
 
-    def form_valid(self, form):
-        post = self.get_object()
-        comentario = Comentario(**form.cleaned_data)
-        comentario.post_comentario = post
+        pk = self.kwargs.get('pk')
+        post = get_object_or_404(Post, pk=pk, publicado_post=True)
+        self.contexto = {
+            'post': post,
+            'comentarios': Comentario.objects.filter(post_comentario=post,
+                                                     publicado_comentario=True),
+            'form': FormComentario(request.POST or None),
+        }
 
-        if self.request.user.is_authenticated:
-            comentario.usuario_comentario = self.request.user
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, self.contexto)
 
+    def post(self, request, *args, **kwargs):
+        form = self.contexto['form']
+
+        if not form.is_valid():
+            return render(request, self.template_name, self.contexto)
+
+        comentario = form.save(commit=False)
+
+        if request.user.is_authenticated:
+            comentario.usuario_comentario = request.user
+
+        comentario.post_comentario = self.contexto['post']
         comentario.save()
-        messages.success(self.request, 'Comentário enviado com sucesso.')
-        return redirect('post_detalhes', pk=post.id)
+        messages.success(request, 'Seu comentário foi enviado para revisão.')
+        return redirect('post_detalhes', pk=self.kwargs.get('pk'))
+
+# class PostDetalhes(UpdateView):
+#     template_name = 'posts/post_detalhes.html'
+#     model = Post
+#     form_class = FormComentario
+#     context_object_name = 'post'
+#
+#     def get_context_data(self, **kwargs):
+#         contexto = super().get_context_data(**kwargs)
+#         post = self.get_object()
+#         comentarios = Comentario.objects.filter(publicado_comentario=True,
+#                                                 post_comentario=post.id)
+#         contexto['comentarios'] = comentarios
+#         return contexto
+#
+#     def form_valid(self, form):
+#         post = self.get_object()
+#         comentario = Comentario(**form.cleaned_data)
+#         comentario.post_comentario = post
+#
+#         if self.request.user.is_authenticated:
+#             comentario.usuario_comentario = self.request.user
+#
+#         comentario.save()
+#         messages.success(self.request, 'Comentário enviado com sucesso.')
+#         return redirect('post_detalhes', pk=post.id)
